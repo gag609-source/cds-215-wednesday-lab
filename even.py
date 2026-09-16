09efeb02a6fae9b1a6aeb7b8471d6d957b94b6c1
gag609-source <gag609@culver.edu>
2026-09-16 15:01:03 -0500

    Update odd number even.py

--- even.py
+++ even.py
@@ -2,9 +2,9 @@ number = int(input("enter a positive number=>"))
 count = 1
 while count <= number:
 	if count %2 == 0:
-	    print(count, " is even by 3")
+	    print(count, " is even")
 else:
-		print(count," is not odd by 3")
+		print(count," is odd number")
 count = count + 1
 
 print("finished")
