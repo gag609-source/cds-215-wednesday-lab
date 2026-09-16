d2c907ad5b1eebfd1ecc4eabc69958308198654e
gag609-source <gag609@culver.edu>
2026-09-16 14:52:56 -0500

    Add files via upload

---
+++ number.py
@@ -0,0 +1,10 @@
+number = int(input("enter a positive number=>"))
+count = 1
+while count <= number:
+	if count %3 == 0
+		print(count, " is divisible by 3")
+	else:
+		print(count," is not divisible by 3")
+	count = count + 1
+
+print("finished")
